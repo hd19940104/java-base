ce4939c5f45af041da4c607e1db38a46b3fa4180
hd19940104 <952542927@qq.com>
2020-08-23 19:17:34 +0800

    修改代码

--- java-base/algorithm/src/main/java/com/zixue/algorithm/thread/DemoAQS.java
+++ java-base/algorithm/src/main/java/com/zixue/algorithm/thread/DemoAQS.java
@@ -1,8 +1,23 @@
 package com.zixue.algorithm.thread;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
 /**
  * AQS
- *
+ *  是指 AbstractQueuedSynchronizer  是一个抽象类
  */
-public class DemoAQS {
+public class DemoAQS extends AbstractQueuedSynchronizer {
+
+    public static void main(String[] args) {
+
+        CountDownLatch countDownLatch=new CountDownLatch(10);
+        Semaphore semaphore=new Semaphore(10);
+        CyclicBarrier cyclicBarrier=new CyclicBarrier(10);
+
+    }
+
+
 }
